1ce96b6437e5854f6f1918bbb8c25015065d84a5
C0ldSn4p <48802423+C0ldSn4p@users.noreply.github.com>
2020-12-18 19:52:43 +0100

    License on files

--- flairData/secret_keys_empty.py
+++ flairData/secret_keys_empty.py
@@ -1,3 +1,15 @@
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
+#
+# @author C0ldSn4p
+
+# template of the secret_keys.py file required for the script
+
+# scope required: modflair flair (modflair%20flair)
+
 conf = {
     "user_agent": "My user Agent",
     "auth_id": "The App Id",
